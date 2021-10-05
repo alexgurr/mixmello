07ce27a05a90ecee0ef4420598faad370cf8529a
alexgurr <me@alexgurr.com>
2021-10-05 17:52:35 +1100

    Fix config process env var

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-	REDIRECT_URL: process.env.REACT_REDIRECT_URL || 'http://localhost:3000',
+	REDIRECT_URL: process.env.REACT_APP_REDIRECT_URL || 'http://localhost:3000',
 };
 
 export default CONFIG;
