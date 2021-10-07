c52293e358399c66242a14a11dfdd557b54406a0
alexgurr <me@alexgurr.com>
2021-10-07 18:25:48 +1100

    Remove commented out icon code

--- src/common/components/Icon/Icon.js
+++ src/common/components/Icon/Icon.js
@@ -6,11 +6,5 @@ export default function Icon({ name, colour, size = 24, brand, spin }) {
 			className={cx('icon', `fa${brand ? 'b' : 's'}`, name, { spin })}
 			style={{ color: colour, fontSize: size }}
 		/>
-		// <FontAwesomeIcon
-		// 	icon={SolidIcons[name] || BrandIcons[name]}
-		// 	color={colour || '#112035'}
-		// 	style={{ fontSize: size || 24 }}
-		// 	{...props}
-		// />
 	);
 }
