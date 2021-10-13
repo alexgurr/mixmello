ed2059e7616babd55cbd55803c438a11e35a27f8
alexgurr <me@alexgurr.com>
2021-10-13 15:46:25 +1100

    Add manifest json + icons script to service worker cache

--- src/service-worker.js
+++ src/service-worker.js
@@ -43,9 +43,14 @@ registerRoute(
 // precache, in this case same-origin .png requests like those from in public/
 registerRoute(
   // Add in any other file extensions or routing criteria as needed.
-  ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'), // Customize this strategy as needed, e.g., by changing to CacheFirst.
+  ({ url }) => (
+    // Logos & manifest
+    (url.origin === self.location.origin && (url.pathname.endsWith('.png') || url.pathname.endsWith('.json'))
+      // Icons
+      || url.href.includes('kit.fontawesome.com')
+  ),
   new StaleWhileRevalidate({
-    cacheName: 'images',
+    cacheName: 'files',
     plugins: [
       // Ensure that once this runtime cache reaches a maximum size the
       // least-recently used images are removed.
